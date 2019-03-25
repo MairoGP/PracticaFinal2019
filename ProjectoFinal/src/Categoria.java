fb8737054ea384d3db5483c7400be42068958ff0
MarioGP <44753637+MairoGP@users.noreply.github.com>
2019-03-25 16:44:23 +0100

    Update Categoria.java

--- ProjectoFinal/src/Categoria.java
+++ ProjectoFinal/src/Categoria.java
@@ -1,3 +1,28 @@
 public enum Categoria {
-
+  ACCION("Acción"),
+  AVENTURAS("Aventuras"),
+  ARCADE("Arcade"),
+  DEPORTIVO("Deportivo"),
+  ESTRATEGIA("Estrategia"),
+  SIMULACION("Simulación"),
+  FIGHTING("Lucha"),
+  PLATAFORMAS("Plataformas"),
+  CONDUCCION("Conducción"),
+  TERROR("Terror"),
+  RPG("Role Play Game"),
+  ACTIONRPG("Action RPG"),
+  MOBA("Multiplayer Online Battle Arena"),
+  SINGLEPLAYER("Un solo jugador"),
+  MULTIPLAYER("Varios jugadores"),
+  HACKNSLASH("Hack and Slash"),
+  MMO("Masive Multiplayer Online),
+  MMORPG("Massive Multiplayer Online Role Play Game"),
+  COOPERATIVO("Cooperativo"),
+  INDIE("Indie"),
+  
+  private final String nombre;
+      
+  Categoria(String s){
+    this.nombre = s;
+  }
 }
